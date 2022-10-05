28377cd263409a3ab065efa890a267da66b65877
RakeshVallabineni <88274977+RakeshVallabineni@users.noreply.github.com>
2022-10-05 15:15:28 +0530

    backend

---
+++ Expense Tracker/controllers/signUpController.js
@@ -0,0 +1,32 @@
+const SIGNUP=require('../models/signUpModel.js');
+
+const bcrypt=require('bcrypt');
+const saltRounds=10;
+
+exports.postUserSignUp=async (req,res,next)=>{
+    try{
+    const requestBody=req.body;
+    console.log(requestBody);
+   
+   bcrypt.hash(requestBody.UPassword,saltRounds,async(err,hash)=>{
+   
+   let signupResponse=await SIGNUP.create({
+    
+    firstname:requestBody.UFirstName,
+    lastname:requestBody.ULastName,
+    emailid:requestBody.UEmail,
+    password:hash
+    })
+    // .then(signup=>{res.status(200).json({SIGNUP:signup});console.log(signup)}).catch(err=>console.log(err));
+    res.status(200).json({SIGNUP:signupResponse})
+    console.log(signupResponse);
+    //    const requestBody=req.body;
+    //    console.log(requestBody);
+})
+}
+catch(err){
+    console.log(err);
+}
+   
+}
+
